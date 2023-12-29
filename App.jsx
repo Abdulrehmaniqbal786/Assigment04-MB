5a9e91311c6ea37e346bd8218116ba41f991c835
Abdulrehmaniqbal786 <147655636+Abdulrehmaniqbal786@users.noreply.github.com>
2023-12-30 02:59:28 +0500

    Complete Work

---
+++ App.jsx
@@ -0,0 +1,140 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div>
+      <nav className='navbar'>
+    <img className='logo' src={"src/Images/favicon.ico"} width={"50px"}   />
+    <button className='signup'>Sign Up</button>
+    
+   </nav>
+
+
+      <img src={"src/Images/bg-masthead.jpg"} className='slider' alt="logo" width={"100%"}  height={"500px"}  />
+      <h1 className='h1'>Generate more leads with a<br></br> professional landing page!</h1>
+      <input className='input' type='email' placeholder='   Email Adress'></input>
+      <button className='submit'>Submit</button>
+
+
+
+      <div className='maindiv'>
+      <div className='div1'>
+      <img  width={"100px"} src={"src/Images/icon1.ico.png"}></img>
+      <h3>Fully Responsive</h3>
+      <p >Lorem ipsum, dolor sit amet consectetur <br></br> adipisicing elit. Voluptas, eos?</p>
+      
+      </div>
+      <div className='div2'>
+      <img  width={"100px"} src={"src/Images/icon2.ico.png"}></img>
+      <h3>Bootrap 5 Ready</h3>
+      <p>Lorem ipsum, dolor sit amet consectetur <br></br> adipisicing elit. Voluptas, eos?</p>
+
+      </div>
+      <div className='div3'>
+      <img width={"100px"} src={"src/Images/icon3.ico.png"}></img>
+      <h3>Easy to Use</h3>
+      <p>Lorem ipsum, dolor sit amet consectetur <br></br> adipisicing elit. Voluptas, eos?</p>
+
+      </div>
+</div>
+
+<div className='main'>
+
+<div className="row1">
+<h2>Fully Responsive Design </h2>
+<p className='p1'>Lorem, ipsum dolor sit amet consectetur adipisicing<br></br> elit. 
+  Quisquam perferendis cupiditate ad, provident<br></br> molestiae voluptas ut qui esse consectetur eligendi.</p>
+<img className='unit' height={"300px"} width={"750px"} src='src/Images/bg-showcase-2.jpg'></img>
+<h2>Easy to Use & Customize</h2>
+<p className='p2'>Lorem, ipsum dolor sit amet consectetur adipisicing<br></br> elit. 
+  Quisquam perferendis cupiditate ad, provident<br></br> molestiae voluptas ut qui esse consectetur eligendi.</p>
+
+</div>
+
+<div className="row2">
+<img height={"300px"} width={"750px"} src='src/Images/bg-showcase-1.jpg'></img>
+<h2>Bootrap 5 Ready </h2>
+<p className='p3'>Lorem, ipsum dolor sit amet consectetur adipisicing<br></br> elit. 
+  Quisquam perferendis cupiditate ad, provident<br></br> molestiae voluptas ut qui esse consectetur eligendi.</p>
+<img className='unit1' height={"300px"} width={"750px"} src='src/Images/bg-showcase-3.jpg'></img>
+</div>
+
+</div>
+      
+<div className='men'>
+
+<div className='men1'>
+      <img className='m1' width={"150px"} src={"src/Images/testimonials-1.jpg"}></img>
+      <h3 className='d1'>Margaret E.</h3>
+      <p>Lorem ipsum dolor Lorem, ipsum dolor amet<br></br>adipisicing.</p>
+      
+      
+      </div>
+      <div className='men2'>
+      <img  className='m2' width={"150px"} src={"src/Images/testimonials-2.jpg"}></img>
+      <h3 className='d2'>Fred S.</h3>
+      <p>Lorem, ipsum dolor sit amet consectetur<br></br> adipisicing elit. Harum,Lorem, ipsum<br></br> dolor. iusto!</p>
+      
+
+      </div>
+      <div className='men3'>
+      <img  className='m3' width={"150px"} src={"src/Images/testimonials-3.jpg"}></img>
+      <h3 className='d3'>Sarah W.</h3>
+      <p>Lorem ipsum, dolor sit amet consectetur<br></br> adipisicing elit. Enim, libero.</p>
+      
+
+</div>
+
+</div>
+      
+<div className='Foo'>
+ 
+
+<img src={"src/Images/bg-masthead.jpg"} className='slider' alt="logo" width={"100%"}  height={"300px"}  />
+<h1 className='end'>Ready to get started?Sign up now!</h1>
+<input className='myinput' type='email' placeholder='  Email Adress'/>
+
+<div>
+  <button className='uni'>Submit</button>
+</div>
+
+
+
+      
+
+
+</div>
+
+
+
+<div className='ic'>
+<a className='md1' href="">About</a>
+<a className='md2' href="">Contact</a>
+<a className='md3' href="">Term of Use</a>
+<a className='md4' href="">Privacy Policy</a>
+<div className='ii'>
+<img className='i1' src="src/Images/loc1.png"  />
+<img className='i2' src="src/Images/loc2.png"  />
+<img className='i3' src="src/Images/loc3.png"  />
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+      
+  )
+}
+
+export default App
---
+++ Component1st.jsx
@@ -0,0 +1,12 @@
+const Component1st = ()=>{
+    return(
+        <div>
+        
+  </div>
+
+
+        
+    )
+}
+
+export {Component1st};
